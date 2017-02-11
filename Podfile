06819128d83628497a360f3c0ff22af1ff772c21
qinge <yuelong.qin@monstar-lab.com>
2017-02-11 14:02:10 +0800

    初始化 note swift 工程

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '9.0'
+target 'note' do
+
+pod 'MMDrawerController', '~> 0.5.7'
+
+end
---
+++ note/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  note
+//
+//  Created by qinge on 17/2/11.
+//  Copyright © 2017年 qin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
